ca225a0a4a25e28d8f1af7cc8412e87cd3f4b01c
musthafa2515 <mutuumusthafa@gmail.com>
2024-01-09 21:53:08 +0530

    counter app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './components/Counter'
+
+
+function App() {
+
+  return (
+    <div style={{width:'100%', height:'100vh' }} className='d-flex justify-content-center align-items-center'>
+    <Counter/>
+    </div>
+  )
+}
+
+export default App
